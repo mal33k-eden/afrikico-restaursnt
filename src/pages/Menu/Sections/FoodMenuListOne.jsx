185276b51f5dda8d39cca5d92c8e82ea14f493a8
Kudafoke isaiah <kudastech@gmail.com>
2025-06-27 15:02:05 +0100

    make adjustment on menu words

--- src/pages/Menu/Sections/FoodMenuListOne.jsx
+++ src/pages/Menu/Sections/FoodMenuListOne.jsx
@@ -69,7 +69,7 @@ const FoodMenuListOne = () => {
     },
     {
       name: "Tofu/Beef /Chicken  Khebab",
-      description: "Tender, marinate Beef ,Chicken or Tofu skewers grilled to perfection over an open flame, served with sautéed peppers and onions. Infused with bold spices and smoky flavor.",
+      description: "Tender, marinate beef, chicken or tofu skewers grilled to perfection over an open flame, served with sautéed peppers and onions. Infused with bold spices and smoky flavor.",
       image: "../images/food-img/dummy.jpg",
       price:12.99,
     },
@@ -81,7 +81,7 @@ const FoodMenuListOne = () => {
     },
     {
       name: "Spicy Goat (Hot)",
-      description: "Spicy hot grilled/fried Goat with peppers, onions, sauce and green peppers.",
+      description: "Spicy hot grilled/fried goat with peppers, onions, sauce and green peppers.",
       image: "../images/food-img/dummy.jpg",
       price:15,
     },
